a87eb149bd6870762cff681dda09a585bfb2b8c1
salomao270 <salomao270@hotmail.com>
2021-01-11 02:06:38 -0300

    release project files

---
+++ WebAPI_example/Controllers/CustomerController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI_example.Models;
+
+namespace WebAPI_example.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        private static List<Customer> customers = new List<Customer>();
+
+        [HttpGet]
+        public List<Customer> GetCustomers()
+        {
+            return customers;
+        }
+
+        [HttpPost]
+        public void InsertCustomer(string _name)
+        {
+            if (!String.IsNullOrEmpty(_name))
+                customers.Add(new Customer(_name));
+        }
+
+        [HttpDelete]
+        public void DeleteCustomer(string _name)
+        {
+            customers.Remove(customers.FirstOrDefault(c => c.Name.Equals(_name)));
+        }
+
+    }
+}
---
+++ WebAPI_example/Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI_example.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public Customer(string _name)
+        {
+            this.Name = _name;
+        }
+    }
+}
